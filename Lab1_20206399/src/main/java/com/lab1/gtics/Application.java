b1cda33a295531824cd7d2f5f717e2a41d1ec20c
Rosell <a20206399@pucp.edu.pe>
2026-08-27 21:33:54 -0500

    lab1

---
+++ Lab1_20206399/src/main/java/com/lab1/gtics/Application.java
@@ -0,0 +1,10 @@
+package com.lab1.gtics;
+
+import org.springframework.boot.SpringApplication; import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
